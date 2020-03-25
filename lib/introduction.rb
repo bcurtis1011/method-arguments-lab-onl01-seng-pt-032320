aa7a83f67a36ca5b0651773c99291871d70e9469
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-03-25 21:04:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,12 +2,12 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction("Josh")
+introduction("name")
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("name", "Ember.js")
+introduction_with_language("name", "language")
 
 
